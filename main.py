6690d43ff005b8e32d89a93a3496283329c8b752
cantelevate <nathanmast@gmail.com>
2020-11-25 10:40:46 -0600

    Initial commit

--- main.py
+++ main.py
@@ -7,3 +7,26 @@ A customer receives 2 points for every dollar spent over $100 in each transactio
 
 Given a record of every transaction during a three month period, calculate the reward points earned for each customer
  per month and total."""
+
+import sqlite3
+import math
+import datetime
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+c.execute(f'select * from transactions where date={datetime.datetime.now().date()}')
+print(c.fetchall())
+
+
+
+def rewards(totalspent):
+    if totalspent <= 50:
+        return 0
+    elif totalspent <= 100:
+        return totalspent - 50
+    else:
+        doublepoints = totalspent - 100
+        return math.floor(50 + 2 * doublepoints)
+
+
+
+
